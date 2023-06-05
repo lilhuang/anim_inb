4abd1d3e2887467f9878862152a3cce5df9d63e9
Lillian Huang <lilhuang@nexuscfar00.umiacs.umd.edu>
2023-06-05 10:21:52 -0400

    cut down dataloaders

--- datas/__init__.py
+++ datas/__init__.py
@@ -1,27 +1,11 @@
-from .AniTripletWithSGMFlow import AniTripletWithSGMFlow
-from .AniTripletWithSGMFlowTest import AniTripletWithSGMFlowTest
 from .BlenderAniTripletPatch import BlenderAniTripletPatch
 from .BlenderAniTripletPatchTest import BlenderAniTripletPatchTest
 from .BlenderAniTripletPatch2 import BlenderAniTripletPatch2
 from .BlenderAniTripletPatchTest2 import BlenderAniTripletPatchTest2
-from .CSVAniTripletWithSGMFlowTest import CSVAniTripletWithSGMFlowTest
-from .CSVAniTripletWithSGMFlow import CSVAniTripletWithSGMFlow
-from .AniTriplet import AniTriplet
-from .MaskToImageLoader import MaskToImageLoader
 from .CSVEXRTriplet import CSVEXRTriplet
-from .CSVEXRQuintlet import CSVEXRQuintlet
-from .CSVEXRQuintletTest import CSVEXRQuintletTest
 
-__all__ = ['AniTripletWithSGMFlowTest', \
-		   'AniTripletWithSGMFlow', \
-		   'BlenderAniTripletPatch', \
+__all__ = ['BlenderAniTripletPatch', \
 		   'BlenderAniTripletPatchTest', \
-		   'CSVAniPairWithSGMFlowTest', \
-		   'CSVAniTripletWithSGMFlow', \
-		   'AniTriplet', \
-		   'MaskToImageLoader', \
 		   'CSVEXRTriplet', \
-		   'CSVEXRQuintlet', \
-		   'CSVEXRQuintletTest', \
 		   'BlenderAniTripletPatch2', \
 		   'BlenderAniTripletPatchTest2']
